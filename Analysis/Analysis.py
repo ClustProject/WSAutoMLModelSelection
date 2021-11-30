20ad60c890f6a9208cc5a55731d0c22d61eefac8
choitaedong2 <72964955+choitaedong2@users.noreply.github.com>
2021-11-30 14:29:28 +0900

    Update Analysis.py

--- Analysis/Analysis.py
+++ Analysis/Analysis.py
@@ -20,7 +20,24 @@ class Analysis:
 
 
 # In[ ]:
+# 학습 완료한 딥러닝 모델 pickle 파일로 변환
+import pickle
+import joblib
 
+def create_pkl_file(inAnal):
+    filename = "anal_result.pkl"
+    # 변수 저장 기반 예측
+    saved_model = pickle.dumps(Analysis.Analysis(inAnal))
+    # Load the pickled model
+    analsys_from_pickle = pickle.loads(saved_model)
+
+    # Use the loaded pickled model to make predictions
+    analsys_from_pickle.predict("predictVal")
+    
+    # 파일 저장 기반 예측
+    joblib.dump(Analysis.Analysis(inAnal), filename)
+    clf_from_joblib = joblib.load(filename) 
+    clf_from_joblib.predict("predictVal")
 
 
 
